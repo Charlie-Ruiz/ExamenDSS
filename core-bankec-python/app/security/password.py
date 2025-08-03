85da8fdb31d5b7c7cad46bf5ccc185a0b67c811a
Karina Arichavala <karina.arichavala@epn.edu.ec>
2025-08-03 09:15:21 -0500

    Cambio de los mensajes de error

--- core-bankec-python/app/security/password.py
+++ core-bankec-python/app/security/password.py
@@ -3,22 +3,22 @@ import re
 def validate_security_password(password: str) ->tuple[bool, list]:
     # 1. Longitud mínima 12
     if len(password) < 12:
-        return False, "Debe tener al menos 12 caracteres."
+        return False, "La contraseña debe tener al menos 12 caracteres."
 
     # 2. Contener minúsculas
     if not re.search(r"[a-z]", password):
-        return False, "Debe incluir al menos una letra minúscula (a-z)."
+        return False, "La contraseña debe incluir al menos una letra minúscula (a-z)."
 
     # 3. Contener mayúsculas
     if not re.search(r"[A-Z]", password):
-        return False, "Debe incluir al menos una letra mayúscula (A-Z)."
+        return False, "La contraseña debe incluir al menos una letra mayúscula (A-Z)."
 
     # 4. Contener dígitos
     if not re.search(r"\d", password):
-        return False, "Debe incluir al menos un número (0-9)."
+        return False, "La contraseña debe incluir al menos un número (0-9)."
 
     # 5. Contener caracteres especiales
     if not re.search(r"[!@#$%^&*(),.?\":{}|<>_\-+=/\\\[\]`~;']", password):
-        return False, "Debe incluir al menos un símbolo especial."
+        return False, "La contraseña debe incluir al menos un símbolo especial."
     
     return True, []
